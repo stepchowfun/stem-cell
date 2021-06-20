6a58c9c2629c1eee89c127619fb49c4b8088fd43
Stephan Boyer <stephan@stephanboyer.com>
2021-06-20 15:20:48 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use clap::App;
+
+// The program version
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+// Let the fun begin!
+fn main() {
+    // Parse the command-line arguments.
+    App::new("Stem Cell")
+        .version(VERSION)
+        .version_short("v")
+        .author("Stephan Boyer <stephan@stephanboyer.com>")
+        .about(
+            "A simple project to demonstrate the cross-platform release management process for my \
+            open source work.",
+        )
+        .get_matches();
+
+    // Greet the user.
+    println!("Hello, World!");
+}
