4fc7f78c11fd4dd8fb2bb18e4528f476286ffecb
Stephan Boyer <stephan@stephanboyer.com>
2025-03-20 02:44:25 -0700

    Install v0.0.8 by default

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/stem-cell"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.0.7}"
+  RELEASE="v${VERSION:-0.0.8}"
 
   # Determine which binary to download.
   FILENAME=''
